0a4d31cb163c777ff3988ed65839aad4e1944d22
polinabojko <polinabojko47@gmail.com>
2026-02-17 14:11:33 +0900

    keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,17 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+def main_menu():
+    markup = InlineKeyboardMarkup()
+    markup.add(
+        InlineKeyboardButton("📝 Задачи", callback_data="tasks"),
+        InlineKeyboardButton("🔁 Привычки", callback_data="habits")
+    )
+    markup.add(
+        InlineKeyboardButton("🎯 Фокус", callback_data="focus"),
+        InlineKeyboardButton("📊 Статистика", callback_data="stats")
+    )
+    markup.add(
+        InlineKeyboardButton("🗒 Заметки", callback_data="notes"),
+        InlineKeyboardButton("😊 Настроение", callback_data="mood")
+    )
+    return markup
