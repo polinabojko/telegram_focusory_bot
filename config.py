e43daef862a19e1a604440899d1587236895b235
polinabojko <polinabojko47@gmail.com>
2026-02-17 14:53:09 +0900

    config.py

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not TOKEN:
+    raise ValueError("BOT_TOKEN не найден в переменных окружения")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL не найден в переменных окружения")
