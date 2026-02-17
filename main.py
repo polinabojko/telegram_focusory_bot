1879af7ec512b8c1f8b256752297dfa3f641366f
polinabojko <polinabojko47@gmail.com>
2026-02-17 14:12:11 +0900

    main.py

--- main.py
+++ main.py
@@ -1,54 +1,31 @@
 import telebot
-from telebot import types
 from config import TOKEN
 from database import init_db
+import keyboards
 import tasks
-import habits
-import focus
-import notes
-import stats
 
 bot = telebot.TeleBot(TOKEN)
+
 init_db()
-
-user_states = {}
-
-def main_menu():
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add("📝 Задачи", "🔁 Привычки")
-    markup.add("🎯 Фокус", "📊 Статистика")
-    markup.add("🗒 Заметки", "😊 Настроение")
-    return markup
 
 @bot.message_handler(commands=["start"])
 def start(message):
     bot.send_message(
         message.chat.id,
-        "Управление задачами и привычками.\nВыберите раздел.",
-        reply_markup=main_menu()
+        "Управление задачами и привычками.",
+        reply_markup=keyboards.main_menu()
     )
 
-@bot.message_handler(func=lambda m: True)
-def router(message):
-    text = message.text
-    user_id = message.chat.id
+@bot.callback_query_handler(func=lambda call: True)
+def callback_router(call):
+    data = call.data
+    user_id = call.message.chat.id
 
-    if text == "📝 Задачи":
-        tasks.menu(bot, user_id)
+    if data == "tasks":
+        tasks.tasks_menu(bot, call.message)
 
-    elif text == "🔁 Привычки":
-        habits.menu(bot, user_id)
-
-    elif text == "🎯 Фокус":
-        focus.menu(bot, user_id)
-
-    elif text == "🗒 Заметки":
-        notes.menu(bot, user_id)
-
-    elif text == "📊 Статистика":
-        stats.show(bot, user_id)
-
-    elif text == "😊 Настроение":
-        stats.mood_menu(bot, user_id)
+    elif data.startswith("tasks_page_"):
+        page = int(data.split("_")[-1])
+        tasks.show_tasks(bot, call.message, page)
 
 bot.polling()
