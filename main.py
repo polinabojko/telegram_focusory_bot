4227894302a63899658eb80226254e06a768c79c
polinabojko <polinabojko47@gmail.com>
2026-02-17 14:19:35 +0900

    main.py

--- main.py
+++ main.py
@@ -27,5 +27,29 @@ def callback_router(call):
     elif data.startswith("tasks_page_"):
         page = int(data.split("_")[-1])
         tasks.show_tasks(bot, call.message, page)
+        
+    elif data == "add_task":
+        tasks.ask_task_text(bot, call)
+        
+    elif data.startswith("due_"):
+        parts = data.split("|")
+        due_type = parts[0].replace("due_", "")
+        title = parts[1]
+        tasks.save_task(user_id, title, due_type)
+        tasks.tasks_menu(bot, call.message)
+
+    elif data.startswith("complete_"):
+        task_id = int(data.split("_")[1])
+        tasks.complete_task(task_id)
+        tasks.show_tasks(bot, call.message, 0)
+
+    elif data.startswith("delete_"):
+        task_id = int(data.split("_")[1])
+        tasks.delete_task(task_id)
+        tasks.show_tasks(bot, call.message, 0)
+
+    elif data.startswith("edit_"):
+        task_id = int(data.split("_")[1])
+        tasks.edit_task(bot, call, task_id)
 
 bot.polling()
